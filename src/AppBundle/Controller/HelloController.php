45f42f6c038289a1c86b3d0ae999bbdf0622dba5
Dmitriy Belgiets <chegidim@gmail.com>
2016-11-14 15:21:54 +0200

    Rendering a Template 2

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -5,14 +5,40 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class HelloController extends Controller
 {
     /**
      * @Route("/hello/{name}", name="hello")
      */
-    public function indexAction($name)
+    public function indexAction($name, Request $request)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        $page = $request->query->get('page', 1);
+
+        return new Response(
+            "<html><body><p>Hello {$name}!</p><p>{$page}</p></body></html>"
+        );
+    }
+
+    /**
+     * @Route("/redirect", name="redirect")
+     */
+    public function redirectAction()
+    {
+        return $this->redirect('http://symfony.com');
+    }
+
+    /**
+     * @Route("/showflash", name="showflash")
+     */
+    public function flashAction()
+    {
+        $this->addFlash(
+            'notice',
+            'This is some notice'
+        );
+
+        return $this->render('@App/hello/hello.html.twig');
     }
 }
