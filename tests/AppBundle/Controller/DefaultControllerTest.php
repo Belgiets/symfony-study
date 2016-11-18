9ba96800ff62f66a49fe71e5eebefd8adaf0ef18
Dmitriy Belgiets <chegidim@gmail.com>
2016-11-18 16:16:19 +0200

    added simple functional test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -15,4 +15,16 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testShowProducts()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/show-products');
+
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("We have")')->count()
+        );
+    }
 }
